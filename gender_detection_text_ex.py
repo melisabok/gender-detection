50c025d894c181a073a1af4ea081899fd10f2ea7
Melisa Bok <melisabok@gmail.com>
2015-10-28 10:36:24 -0400

    use pipeline

--- gender_detection_text_ex.py
+++ gender_detection_text_ex.py
@@ -6,8 +6,10 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+from sklearn.pipeline import Pipeline
 
 
 
@@ -28,7 +30,7 @@ class GenderDetectionTextEx:
         for (coef_1, fn_1), (coef_2, fn_2) in top:
             print "\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2)
 
-    def execute(self, tweets, categories):
+    def execute(self, classifier, tweets, categories):
 
         data = []
         target = []
@@ -39,32 +41,32 @@ class GenderDetectionTextEx:
 
         data_train, data_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
 
-        vectorizer = TfidfVectorizer(ngram_range=(1,6), tokenizer = self.tokenizer.tokenize, stop_words=['user', '#tag', 'url', '#userconjustinbieber'], strip_accents='unicode', min_df=15)
-        print len(data_train)
-        X_train = vectorizer.fit_transform(data_train)
+        
+        vectorizer = TfidfVectorizer(ngram_range=(1,6), tokenizer = self.tokenizer.tokenize, stop_words=['user', '#tag', 'url', '#userconjustinbieber'], strip_accents='unicode', min_df=10)
+        
+        pipeline = Pipeline([('feature_extraction', vectorizer), ('classifier', classifier)])
 
-        feature_names = vectorizer.get_feature_names()
+
+        #feature_names = vectorizer.get_feature_names()
 
         #####Save to file
-        df = pd.DataFrame(X_train.sum(axis=0).transpose(), index=feature_names, columns=['Frequency'])
-        sorted_df = df.sort(['Frequency'], ascending=False)
+        #df = pd.DataFrame(X_train.sum(axis=0).transpose(), index=feature_names, columns=['Frequency'])
+        #sorted_df = df.sort(['Frequency'], ascending=False)
 
-        sorted_df.to_csv('features.csv', encoding='utf-8')
+        #sorted_df.to_csv('features.csv', encoding='utf-8')
         #####
 
-        nb = MultinomialNB(alpha=.01)
-        nb.fit(X_train, y_train)
+        pipeline.fit(data_train, y_train)
 
 
-        X_test = vectorizer.transform(data_test)
-        s = nb.score(X_test, y_test)
+        s = pipeline.score(data_test, y_test)
         print s
         
-        pred = nb.predict(X_test)
+        pred = pipeline.predict(data_test)
         score = metrics.f1_score(y_test, pred)
         print("f1-score:   %0.3f" % score)
 
-        self.show_most_informative_features(nb, vectorizer)
+        #self.show_most_informative_features(classifier, vectorizer)
  
 
     def start(self):
@@ -82,10 +84,29 @@ class GenderDetectionTextEx:
             'Male': 0
         }
 
-        self.execute(tweets, categories)
-        self.execute(tweets, categories2)
-        
+        for i in range(0, 10, 1):
+            alpha = i / 10.0
+            nb = MultinomialNB(alpha=alpha)
+            print "MultinomialNB Male, alpha: " + str(alpha)
+            self.execute(nb, tweets, categories)
+            print "MultinomialNB Female, alpha: " + str(alpha)
+            self.execute(nb, tweets, categories2)
 
+        for i in range(0, 10, 1):
+            alpha = i / 10.0
+            nb = BernoulliNB(alpha=alpha)
+            print "BernoulliNB Male, alpha: " + str(alpha)
+            self.execute(nb, tweets, categories)
+            print "BernoulliNB Female, alpha: " + str(alpha)
+            self.execute(nb, tweets, categories2)
+
+        for i in range(0, 10, 1):
+            alpha = i / 10.0
+            svc = LinearSVC(C=alpha, loss='l2', penalty='l2', dual=False, tol=1e-3)
+            print "LinearSVC Male, C: " + str(alpha)
+            self.execute(nb, tweets, categories)
+            print "LinearSVC Female, C: " + str(alpha)
+            self.execute(nb, tweets, categories2)
 
 if __name__ == '__main__':
 
